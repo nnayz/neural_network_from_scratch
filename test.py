134fd40dfe837bcf7926c2062fb71158d41df65f
Nasrul Huda <nasrul2001@gmail.com>
2025-06-21 12:03:08 +0200

    Testing gradients

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
+# %%
 import torch
-import numpy as np
+# import numpy as np
 
-x = np.arange(1, 5, 1)
-y = np.arange(5, 1, -1)
-tensor_x = torch.Tensor(x)
-tensor_y = torch.Tensor(y)
+x = torch.Tensor([2.0, 3.0], requires_grad=True)
+y = torch.Tensor([4.0, 5.0], requires_grad=True)
 
-# print(f"Newly created tensor: {tensor_x}")
-# print(f"The Shape of the tensor: {tensor_x.shape}")
-#
-print(f"{tensor_x * tensor_y}")
+z = (x * y + x)
+loss = z * torch.Tensor(2.0)
+
+# %%
+print(f"Loss: {loss} | Grad: {x.grad}")
--- torch/__init__.py
+++ torch/__init__.py
@@ -2,11 +2,12 @@
 import numpy as np
 
 class Tensor:
-    def __init__(self, x: np.ndarray, requires_grad=False):
-        self.data = np.asarray(x)
+    def __init__(self, x, dtype=np.float32, requires_grad=False):
+        self.data = np.asarray(x, dtype=dtype)
         self.requires_grad = requires_grad
         self.grad = None # Will store the gradient computed
         self._prev = set() # Parent tensors
+        self._backward = lambda : None
 
     @property
     def shape(self):
@@ -21,6 +22,13 @@ class Tensor:
         output = Tensor(self.data + operand.data)
         output._prev = {self, operand}
 
+        def _backward():
+            if self.requires_grad:
+                self.grad = (self.grad or np.zeros_like(self.data)) + np.ones_like(self.data)
+            if operand.requires_grad:
+                operand.grad = (operand.grad or np.zeros_like(operand.data)) + np.ones_like(operand.data)
+
+        output._backward = _backward()
         return output
 
     def __mul__(self, operand):
@@ -28,4 +36,11 @@ class Tensor:
         output = Tensor(self.data * operand.data)
         output._prev = {self, operand}
 
+        def _backward():
+            if self.requires_grad:
+                self.grad = (self.grad or np.zeros_like(self.data)) + operand.data
+            if operand.requires_grad:
+                operand.grad = (operand.grad or np.zeros_like(operand.data)) + self.data
+
+        output._backward = _backward
         return output
